a7f91ef178b0db93a0feba6cc5960a55320eebe7
Aluno <lucasmebo@hotmail.com>
2022-08-03 22:19:11 -0300

    Primeira aula de back-end

---
+++ Server.js
@@ -0,0 +1,24 @@
+// Importa o aquivo express no arquivo js
+import express from 'express'
+import dotenv from 'dotenv'
+
+// Load Config
+dotenv.config({path: './config/config.env'})
+
+// Atribiu a função express para dentro de uma variavel // Run server
+const Server = express()
+// Cria uma variavel e acessa os aquivos .env atraves do process.env para armazenar o numero na porta definida no arquivo.env
+const port = process.env.PORT || 3000
+
+// Usa a função listen que é ouvir, atribui a porta 3000 e colocar uma função ou uma mensagem pra saber se deu certo
+Server.listen(port, console.log("Servidor rodando na porta " + port + "..."))
+
+// primeiros diz qual a rota que é a /, e depois cria uma função, que cria a req que ve o que foi enviado, e a res que responde
+Server.get("/", (req, res) => {
+    // diz que a resposta ira aparecer um texto na tela inicial
+    res.send("Página Inicial")
+})
+
+Server.get("/user", (req, res) => {
+    res.send("Página do Usuário")
+})
