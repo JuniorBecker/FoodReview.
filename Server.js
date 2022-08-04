a26e52a5fa53a4b124f0ecf80b4cdc063083876c
JuniorBecker <joaobeckerjr@outlook.com>
2022-08-04 20:35:25 -0300

    first commit

--- Server.js
+++ Server.js
@@ -8,10 +8,10 @@ dotenv.config({path: './config/config.env'})
 // Atribiu a função express para dentro de uma variavel // Run server
 const Server = express()
 // Cria uma variavel e acessa os aquivos .env atraves do process.env para armazenar o numero na porta definida no arquivo.env
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 // Usa a função listen que é ouvir, atribui a porta 3000 e colocar uma função ou uma mensagem pra saber se deu certo
-Server.listen(port, console.log("Servidor rodando na porta " + port + "..."))
+Server.listen(port, console.log("Servidor rodando em " + process.env.NODE_ENV + " na porta " + port + "..."))
 
 // primeiros diz qual a rota que é a /, e depois cria uma função, que cria a req que ve o que foi enviado, e a res que responde
 Server.get("/", (req, res) => {
